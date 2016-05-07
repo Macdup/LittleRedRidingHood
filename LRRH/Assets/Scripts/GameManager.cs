e66afa7aadadcd601adb38ac3ffa748d33313eca
OneOne <mika.23.joo@gmail.com>
2016-05-07 16:09:07 +0200

    Reset Button

---
+++ LRRH/Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	public ButtonScript BSReset;  
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Backspace) || BSReset.CurrentState == ButtonScript.ButtonState.Down) {  
+			SceneManager.LoadScene (0);  
+		} 
+	}
+
+}
