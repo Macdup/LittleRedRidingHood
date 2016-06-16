2cee911bd9aa0a68cabb3a006321fb029d9980de
Macdup <dupont.marcantoine.isart@gmail.com>
2016-06-16 20:44:04 +0200

    Enemy IA test
    Mis en place d'un ennemi pour tester le contre.

---
+++ LRRH/Assets/Scripts/Player/IAAnim.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IAAnim : MonoBehaviour
+{
+
+    private IATest iaScript;
+    private Spell spellScript; 
+
+	// Use this for initialization
+	void Start () {
+        iaScript = GetComponentInParent<IATest>();
+        //spellScript = playerScript.CurrentSpell;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void destroy(){
+		GameObject.Destroy(transform.parent.gameObject);
+	}
+
+    public void idleEnd() {
+        iaScript.SetIdle(false);
+    }
+
+    public void SetComboPossibility() {
+        iaScript.SetComboPossibility();
+    }
+
+    public void ResetComboValidated() {
+        iaScript.ResetComboValidated();
+    }
+
+    public void ComboCheck()
+    {
+        iaScript.ComboCheck();
+    }
+
+    public void ResetBeingHit() {
+        iaScript.ResetBeingHit();
+    }
+
+    public void ResetUsingMagick()
+    {
+        iaScript.UsingMagic = false;
+    }
+    public void SpellFX()
+    {
+        spellScript.SpellFX();
+    }
+
+}
