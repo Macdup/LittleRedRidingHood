25e29c43e1c20715979ef9bb4aab6ee3c87e9ea0
Macdup <dupont.marcantoine.isart@gmail.com>
2016-09-08 00:11:00 +0200

    correction erreurs + hidden walls creator

--- LRRH/Assets/Scripts/Enemy/Bat.cs
+++ LRRH/Assets/Scripts/Enemy/Bat.cs
@@ -14,9 +14,6 @@ public class Bat : Enemy {
 	private Vector3 dirVector = Vector3.zero;
 
 
-	// Use this for initialization
-	void Start () {
-	}
 	
 	// Update is called once per frame
 	void Update () {
