00c105bb087e43eb34756a83e8efc28cfe3a20d1
Macdup <dupont.marcantoine.isart@gmail.com>
2016-09-19 23:49:17 +0200

    Bomb V1

---
+++ LRRH/Assets/Scripts/Enemy/Bomb.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bomb : MonoBehaviour {
+
+    public float BombTimer;
+
+    private Animator m_Animator;
+    private int _tickingHash = Animator.StringToHash("ticking");
+    private bool _isTicking = false;
+
+    // Use this for initialization
+    void Start () {
+        m_Animator = this.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (_isTicking == true)
+            return;
+
+        BombTimer -= 1 * Time.deltaTime;
+        
+        if (BombTimer < 0) {
+            _isTicking = true;
+            Debug.Log(BombTimer);
+            m_Animator.SetTrigger(_tickingHash);
+        }
+	}
+
+    public void Disable() {
+        transform.gameObject.SetActive(false);
+    }
+}
---
+++ LRRH/Assets/Scripts/Enemy/Grenadier.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using AssemblyCSharp;
+
+public class Grenadier : Enemy {
+
+    public void throwGrenade() {
+
+    }
+	
+}
